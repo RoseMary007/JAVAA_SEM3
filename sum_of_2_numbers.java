7e7e43d25b23d57f2576c5cbdcc83b98f69d7120
RoseMary007 <rosemaryjoe2028@cs.sjcetpalai.ac.in>
2025-07-16 10:58:47 +0530

    Add files via upload

---
+++ sum_of_2_numbers.java
@@ -0,0 +1,15 @@
+package MODULE_1;
+import java.util.*;
+public class sum_of_2_numbers {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		int number1;
+		int number2;
+		int sum;
+		System.out.println("enter two numbers ");
+		number1=sc.nextInt();
+		number2=sc.nextInt();
+		sum=number1+number2;
+		System.out.println("sum is "+sum);
+	}
+}
